78b438a94df50c56be28c389bbfa67622b06f7d7
Lavkush77-web <lavkushkamar7@gmail.com>
2024-09-22 22:32:43 +0530

    commit on 22092024

---
+++ src/sept22092024/Lab001.java
@@ -0,0 +1,14 @@
+package sept22092024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        int a=50;
+        int b=70;
+        int c=45;
+        a= a>b ? a : b;
+        c=a>c?a:c;
+        System.out.println(c);
+
+
+    }
+}
